5c4a55cc1bec46c5687ec2f70f11f874a24a0629
Mikaela <gardemikaela@gmail.com>
2020-02-18 18:33:36 +0100

    .

--- Labbar/Num1/Övning/ljud.py
+++
@@ -1,9 +0,0 @@
-import scipy.io.wavfile
-import scipy.fftpack
-import matplotlib.pyplot as plt
-
-rate, data = scipy.io.wavfile.read("Piano_2.wav")
-fft = scipy.fftpack.fft(data)
-
-plt.plot(abs(fft))
-plt.show()
